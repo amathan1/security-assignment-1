08fdb9db3641332672852b2fce35ffe6ecd715e6
Ved Ranade <vedranade@VED-SAGER.localdomain>
2018-02-14 18:02:36 -0500

    Server can receive multiple commands from client and print them in its own terminal

---
+++ telnet_client2.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <time.h>
+#include<unistd.h>
+
+int main(int argc, char **argv)
+{
+	int  sockfd, n, bytes_written, bytes_read;
+	char recvline[100], sent_data[100];
+	struct sockaddr_in servaddr;
+
+	if ( argc != 2 )
+	{
+		printf("Usage : gettime <IP address>");
+		exit(1);
+	}
+
+	/* Create a TCP socket */
+	if ( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0 )
+	{
+		perror("socket"); exit(2);
+	}
+
+	/* Specify server’s IP address and port */
+	bzero(&servaddr, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(10000); /* daytime server port */
+
+	if ( inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0 )
+	{
+		perror("inet_pton"); exit(3);
+	}
+
+	/* Connect to the server */
+	if ( connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr))
+		< 0 )
+	{
+		perror("connect"); exit(4);
+	}
+
+	do
+	{
+		printf("\ntelnet> ");
+		scanf("%s", sent_data);
+		
+		//Send to server section
+		if ( strcmp(sent_data, "exit") == 0 )
+		{
+			exit(0);
+		}
+		else
+		{
+			bytes_written = write(sockfd, sent_data, strlen(sent_data));
+			if ( bytes_written == -1 )
+				perror("\nError sending data");
+		}
+
+		//Receive from server section
+	}
+	while ( 1 );
+
+	close(sockfd);
+}
+
---
+++ telnet_server2.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <time.h>
+
+int main(int argc, char **argv)
+{
+	int   listenfd, connfd, clilen, n;
+	struct sockaddr_in servaddr, cliaddr;
+	char buff[100], data_received[100], sent_data[100];
+	time_t ticks;
+
+	/* Create a TCP socket */
+	listenfd = socket(AF_INET, SOCK_STREAM, 0);
+
+	/* Initialize server's address and well-known port */
+	bzero(&servaddr, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddr.sin_port = htons(10000);   /* daytime server */
+
+										/* Bind server’s address and port to the socket */
+	bind(listenfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
+	/* Convert socket to a listening socket – max 100 pending clients*/
+	listen(listenfd, 100);
+
+	for ( ; ; )
+	{
+		/* Wait for client connections and accept them */
+		clilen = sizeof(cliaddr);
+		connfd = accept(listenfd, (struct sockaddr *)&cliaddr, &clilen);
+
+		do
+		{
+			//Receive from client section
+			n = read(connfd, data_received, 100);
+			data_received[n] = '\0';
+			printf("%s\n", data_received);
+			/*if ( (strncmp(data_received, "ls", 2)) == 0 )
+			{
+				printf("\nYOU SUCK");
+			}*/
+
+			//Reply to client section
+
+		}
+		while ( n != 0 );
+		
+		/* Close the connection */
+		close(connfd);
+	}
+}
+
