6120f59e8826ce97d73d52b66ac190bc2af6b235
Arvind Mathanagopalan <amathan1@binghamton.edu>
2018-02-18 18:41:40 -0500

    Done with task_2

--- task_2.cpp
+++ task_2.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <fstream>	// For FD
 #include <cstdlib>	// This has atoi
-#include <string>
+#include <string.h>
 #include <array>
 #include <linux/mman.h>
 #include <vector>
@@ -20,25 +20,19 @@ int
 generate_map(int seed)
 {
 
-
-	cout << "We're coming inside this function" << endl;
-
 	int current;
 	int cur_num;
 	int subs_cnt = 0;
 	while (seed > 0)
 	{
-		cout << "We're inside the while loop" << endl;
 		current = seed % 10;
 		cur_num = alph[current];
 		seed /= 10;
 		subs.push_back(cur_num);
-		inv_map[cur_num-*fst] = *fst + subs_cnt;
+		inv_map[cur_num - *fst] = *fst + subs_cnt;
 		alph.erase(alph.begin() + current);
 		subs_cnt++;
 	}
-
-	cout << "alphabets size is: " << alph.size() << endl;
 
 	for(int i = 0; i < alph.size(); i++)
 	{
@@ -85,24 +79,55 @@ main(int argc, char* argv[])
 	*fst = (int)p;
 	*ptr = 0;
 
-	ofstream input;
-	input.open(argv[1]);
+	cout << "The integer value of a is : " << *fst << endl;
+
+	ifstream is(argv[1]);
+	ofstream os(argv[2]);
+
+	char input_file[10000];
+
+	char c;
+	int cnt = 0;
+
+	while(is.get(c))
+	{
+		input_file[cnt] = c;
+		cout << c;
+		cnt++;
+	}
+
+	char *output_file = (char*)calloc(cnt, sizeof(int)); 
 
 	for (int i = 0; i < 26; ++i)
 	{
 		alph.push_back(*fst+i);
 	}
 
+
 	srand(atoi(argv[3]));
 	seed = rand();
-	cout << "Are we even coming here? " << endl;
 	generate_map(seed);	// This should generate the map and keep it in the subs thing. You know what to do. 
+
+
+	if (atoi(argv[4]) == 1) {
+		for (int i = 0; i < cnt; i++) {
+			output_file[i] = subs[int(input_file[i]) - *fst];
+		}
+	}	else {
+			for (int i = 0; i < cnt; i++) {
+				output_file[i] = inv_map[int(input_file[i]) - *fst];
+		}
+	}
+
+	os << output_file << endl;
+
+
 	
 	for (int i = 0; i < 26; i++) {
-		cout << "\tCipher: " << (char)subs[i] << endl;
+		cout << "Alphabet: " << char(*fst + i) << "\tCipher: " << (char)subs[i] << "\t" << (char)inv_map[i] << endl;
 	}
-	cout << "Okay. So, map has been generated" << endl;
 
+	delete fst, ptr;
 
 	return 0;
 }
